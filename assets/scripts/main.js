9bd68631b6e8cedce99c821c6e6407e58f534c80
demimao <zmao@ucsd.edu>
2022-11-09 19:37:43 -0800

    Update main.js for explore

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -44,15 +44,32 @@ function initializeServiceWorker() {
   /*******************/
   // We first must register our ServiceWorker here before any of the code in
   // sw.js is executed.
-  // B1. TODO - Check if 'serviceWorker' is supported in the current browser
-  // B2. TODO - Listen for the 'load' event on the window object.
+  // B1. DONE - Check if 'serviceWorker' is supported in the current browser
+  if (!("serviceWorker" in navigator)) {
+    console.log("serviceWorker not supported in current browser");
+    return;
+  }
+  // B2. DONE - Listen for the 'load' event on the window object.
   // Steps B3-B6 will be *inside* the event listener's function created in B2
-  // B3. TODO - Register './sw.js' as a service worker (The MDN article
-  //            "Using Service Workers" will help you here)
-  // B4. TODO - Once the service worker has been successfully registered, console
-  //            log that it was successful.
-  // B5. TODO - In the event that the service worker registration fails, console
-  //            log that it has failed.
+  window.addEventListener('load', async () => {
+    // B3. DONE - Register './sw.js' as a service worker (The MDN article
+    //            "Using Service Workers" will help you here)
+    try {
+      const registration = await navigator.serviceWorker.register("LAB7_STARTER/sw.js", {
+
+      });
+      // B4. DONE - Once the service worker has been successfully registered, console
+      //            log that it was successful.
+      if (registration.active) {
+        console.log("Service worker installed and active");
+      }
+    }
+    // B5. DONE - In the event that the service worker registration fails, console
+    //            log that it has failed. 
+    catch (error) {
+      console.error(`Registration failed with ${error}`);
+    }
+  });
   // STEPS B6 ONWARDS WILL BE IN /sw.js
 }
 
